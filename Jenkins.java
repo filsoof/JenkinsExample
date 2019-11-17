2cc07beb2ff6db2465944cb3e6b41f7d72face2c
fredericfilsoof <filsoof95@hotmail.com>
2019-11-17 22:47:15 +0100

    lavede en fejl med vilje

--- Jenkins.java
+++ Jenkins.java
@@ -2,8 +2,8 @@ public class Jenkins {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <10 ; i++) {
-            System.out.println("hello World "+i);
+        for sds(int i = 1; i <10 ; i++) {
+          asda  System.out.println("hello World "+i);ad
         }
 
 
