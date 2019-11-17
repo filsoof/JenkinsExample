ae2c90126603525e122f1bd6d7ebb06fb69f0b3b
fredericfilsoof <filsoof95@hotmail.com>
2019-11-17 22:49:25 +0100

    fixede fejl

--- Jenkins.java
+++ Jenkins.java
@@ -2,8 +2,8 @@ public class Jenkins {
 
     public static void main(String[] args) {
 
-        for sds(int i = 1; i <10 ; i++) {
-          asda  System.out.println("hello World "+i);ad
+        for (int i = 1; i <10 ; i++) {
+            System.out.println("hello World "+i);
         }
 
 
